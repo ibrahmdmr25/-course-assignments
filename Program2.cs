71d0fd56d7a0d6f4fee83fc6b8f4d7242f9a779f
ibrahmdmr25 <ibrahmdmr07@gmail.com>
2024-10-11 23:28:25 +0300

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+static void Main()
+{
+   //Üçgene ait üç kenar ölçüsü giriniz
+   Console.WriteLine("Birinci kenar bilgisini giriniz:");
+   int kenar1 = Convert.ToInt16(Console.ReadLine());
+   Console.WriteLine("İkinci kenar bilgisini giriniz:");
+   int kenar2 = Convert.ToInt16(Console.ReadLine());
+   Console.WriteLine("Üçüncü kenar bilgisini giriniz:");
+   int kenar3 = Convert.ToInt16(Console.ReadLine());
+
+   //Verilen bilgilerle üçgenin çevresinİ hesapla
+   int toplam =  kenar1 + kenar2 + kenar3;
+
+   //Bir kenar bilgisi giriniz 
+   Console.WriteLine("Kenar bilgisini giriniz: ");
+   int kenar = Convert.ToInt16(Console.ReadLine());
+
+   //Verilen bilgiyle karenin alanını hesapla
+   int toplam2 = kenar*kenar;
+   
+   //Verilen bilgileri ekrana yazdır 
+    Console.WriteLine("Üçgenin çevresi : " + toplam);
+    Console.WriteLine("Karenin alanı : " + toplam2);
+}
+}
