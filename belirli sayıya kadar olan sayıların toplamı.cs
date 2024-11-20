7a4777512d632ffd85ecb2c156df94001f4726bd
İbrahim Demir <ibrahmdmr07@gmail.com>
2024-11-20 22:43:44 +0300

    Add files via upload

---
+++ belirli sayıya kadar olan sayıların toplamı.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+        int toplam = 0;
+
+        for (int i = 1; i <= sayi; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine($"{sayi}  Sayıların toplamı: {toplam}");
+    }
+}
+        
