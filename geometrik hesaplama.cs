13a7fb13ed7e53609d1781576b4a77635e9a3a03
İbrahim Demir <ibrahmdmr07@gmail.com>
2024-11-20 22:17:06 +0300

    Add files via upload

---
+++ geometrik hesaplama.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Kullanıcıdan iki sayı al
+        Console.WriteLine("Birinci sayıyı giriniz: ");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("İkinci sayıyı giriniz: ");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        // İki sayıyı topla 
+        int toplam = sayi1 + sayi2;
+        
+        // Sonucu ekrana yazdır
+        Console.WriteLine("Toplam: " + toplam/2);
+    }
+}
+
